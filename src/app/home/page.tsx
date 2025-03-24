1248e4713722b532f69e8b7bb662597d72f62aa2
daestrada22 <daestrada98@gmail.com>
2025-03-23 20:25:24 -0700

    add prettier

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
-  return (
-    <div className="text-center">
-        <h1 className="text-3xl font-bold">Home page</h1>
-        <p>
-            Hello, tennis world!
-        </p>
-    </div>
-  )
+    return (
+        <div className="text-center">
+            <h1 className="text-3xl font-bold">
+                Home page
+            </h1>
+            <p>
+                Hello, tennis world!
+            </p>
+        </div>
+    )
 }
